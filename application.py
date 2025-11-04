f17b35b2b618c1c1e219ad57d98cabd364ad3259
dhanushkodi15 <gdevkodi15@gmail.com>
2025-11-04 18:51:58 +0530

    AWS Deployment

--- application.py
+++ application.py
@@ -35,4 +35,4 @@ def api_predict():
         return jsonify({'success': False, 'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
